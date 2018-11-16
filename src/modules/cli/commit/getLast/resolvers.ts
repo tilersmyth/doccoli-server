38957bc5aa0595d927afd99b3155a1e301170406
Tyler Smith <tyler.smith.la@gmail.com>
2018-11-16 14:49:13 -0500

    simplified TypeORM findOne, added branch to params
    Branch will be taken into consideration to ensure user is publishing intended content

--- src/modules/cli/commit/getLast/resolvers.ts
+++ src/modules/cli/commit/getLast/resolvers.ts
@@ -1,23 +1,20 @@
-import { getConnection } from "typeorm";
-
 import { ResolverMap } from "../../../../types/graphql-utils";
 import { Commit } from "../../../../entity/Commit";
 
 export const resolvers: ResolverMap = {
   Query: {
-    cliLastCommit: async (_, __, { project }: any) => {
+    cliLastCommit: async (_, { branch }: any, { project }: any) => {
       try {
         if (!project) {
           throw "project not found";
         }
 
-        return await getConnection()
-          .createQueryBuilder()
-          .select("commit")
-          .from(Commit, "commit")
-          .where("commit.project = :project", { project: project.id })
-          .orderBy("commit.createdAt", "DESC")
-          .getOne();
+        return await Commit.findOne({
+          where: { project: project.id, branch },
+          order: {
+            createdAt: "DESC"
+          }
+        });
       } catch (e) {
         console.log("server error");
         return null;
